22cb9d7bde9cdd577c4a1d3ccf102c46251ce973
pizzaface_glb_pro <pizzaface_glb_pro@live.com>
2022-04-10 11:18:30 +0800

    hot-fix commit 1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("commit again");
+        System.out.println("hello git4");
     }
 }
