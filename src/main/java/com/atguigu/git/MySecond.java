4df69639e9c905d163e58088938780b508976655
pizzaface_glb_pro <pizzaface_glb_pro@live.com>
2022-04-10 11:10:32 +0800

    third commit

--- src/main/java/com/atguigu/git/MySecond.java
+++ src/main/java/com/atguigu/git/MySecond.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class MySecond {
     public static void main(String[] args) {
         System.out.println("Hello second");
+        System.out.println("hello third");
     }
 }
